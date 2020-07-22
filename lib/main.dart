11baa1b63fb0d9a104e3f4f4f698927ca4993de6
Eugênio Barros <eugenio.barros.jr@gmail.com>
2020-07-22 17:15:29 -0300

    core: Add Files

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'I am Poor App',
+      home: Scaffold(),
+    );
+  }
+}
