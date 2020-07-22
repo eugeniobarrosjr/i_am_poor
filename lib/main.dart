91236e9803dc0a7787b0cad252a075b463b89506
Eugênio Barros <eugenio.barros.jr@gmail.com>
2020-07-22 17:35:15 -0300

    feature: finish app

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,21 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'I am Poor App',
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      title: 'I am Poor',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Poor'),
+          backgroundColor: Colors.teal[700],
+        ),
+        backgroundColor: Colors.lightBlue[300],
+        body: Center(
+          child: Image.network(
+            'http://getdrawings.com/free-icon-bw/coal-icon-1.png',
+            height: 300,
+          ),
+        ),
+      ),
     );
   }
 }
